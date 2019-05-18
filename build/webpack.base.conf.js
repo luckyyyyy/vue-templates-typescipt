d12fdb3bb4b3d89cd0449001c5d79d2d76318f81
bowen <z.bowen66@gmail.com>
2019-05-18 12:08:17 +0800

    perf: keep filename stable

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -17,6 +17,8 @@ const config = require('./config');
 const splitChunks = require('./config/splitChunks');
 const ts = require('typescript');
 console.log('TypeScript Version: ' + ts.version );
+const seen = new Set();
+const nameLength = 4;
 const webpackConfig = {
   entry: {
     app: './src/main.ts',
@@ -110,8 +112,23 @@ const webpackConfig = {
         return env;
       })(),
     }),
-    // keep module.id stable when vendor modules does not change
-    new webpack.NamedChunksPlugin(),
+    // https://github.com/vuejs/vue-cli/issues/1916#issuecomment-407693467
+    // https://segmentfault.com/a/1190000015919928#articleHeader10
+    new webpack.NamedChunksPlugin(chunk => {
+      if (chunk.name) {
+        return chunk.name;
+      }
+      const modules = Array.from(chunk.modulesIterable);
+      if (modules.length > 1) {
+        const hash = require('hash-sum');
+        const joinedHash = hash(modules.map(m => m.id).join('_'));
+        let len = nameLength;
+        while (seen.has(joinedHash.substr(0, len))) len++;
+        seen.add(joinedHash.substr(0, len));
+        return `chunk-${joinedHash.substr(0, len)}`;
+      }
+      return modules[0].id;
+    }),
     new webpack.HashedModuleIdsPlugin(),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
